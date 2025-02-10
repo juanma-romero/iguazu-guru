b5cddcae56802c15952494ba41acbbdcaaa56fbd
Juan Manuel Romero <xjuanma.romerox@gmail.com>
2025-02-10 09:57:09 -0300

    pagina en construccion

--- src/app/[locale]/components/header/Header.tsx
+++ src/app/[locale]/components/header/Header.tsx
@@ -7,11 +7,10 @@ export default function Header() {
     <header className="px-6 pt-8 pb-4">
       <div className="flex items-center justify-between mb-4">
       <Image
-          src="/logo1.png" // Reemplaza con la ruta correcta a tu logo en la carpeta 'public'
+          src="/favicon.png"
           alt="Logo de Iguazu Guru"
-          width={50} // Ajusta el ancho según necesites
-          height={50}  // Ajusta la altura según necesites
-          //className="h-8 w-auto"  Opcional: ajusta el tamaño con clases de Tailwind (h-8 es un ejemplo)
+          width={50} 
+          height={50}           
         />
         <div className="flex items-center space-x-4"> 
           <CurrencySwitcher />
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,15 @@
-import Main from './components/Main';
+//import Main from './components/Main';
+import VideoBackground from './components/Construccion'
  
 export default function HomePage() {
   
   return (        
-      <Main />     
+      //<Main />   
+      <VideoBackground
+        videoMp4="/videos/catas.mp4"        
+        //posterImage="/costadoDerecho.jpg"
+        title="En Construcción"
+        subtitle="¡Volveremos pronto con algo genial!"
+      /> 
   )
 }
